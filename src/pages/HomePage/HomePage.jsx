10ebf1660a61da794f77f54b8970491446b70ca4
gocha001 <kalianichgocha@ukr.net>
2025-01-28 21:33:36 +0200

    add home and favicon

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,19 @@
-
+import css from "./HomePage.module.css";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
+    <div className={css.homeContainer}>
+      <div className={css.homeText}>
+        <div className={css.homeTitleContainer}>
+          <h1 className={css.homeTitle}>Campers of your dreams</h1>
+          <h3 className={css.homeSubTitle}>
+            You can find everything you want in our catalog
+          </h3>
+        </div>
+        <button className={css.homeBtn}>View Now</button>
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
